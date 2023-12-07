d16d6e1d78122e76cb4f047632b80442ee04a97e
MinhSang97 <minhsangnguyen463@gmail.com>
2023-12-07 15:31:32 +0700

    API_HHM

--- middleware/CheckToken.go
+++ middleware/CheckToken.go
@@ -4,6 +4,7 @@ import (
 	"app/model"
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -31,5 +32,29 @@ func ValidToken(ctx context.Context, db *gorm.DB, API_User, API_PassWord string)
 		}
 
 		c.AbortWithStatusJSON(401, gin.H{"error": "authentication failed, check your username and password"})
+	}
+}
+
+func get_token_expire_time(ctx context.Context, db *gorm.DB, token string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var tokens []model.Tokens
+		var expireDate string // Declare expire_date variable
+
+		if err := db.Select("expire_date").Where("token = ?", token).
+			Find(&tokens).
+			Error; err != nil {
+			// Handle the error
+			fmt.Println("Error fetching token expiration time:", err)
+			c.AbortWithStatusJSON(500, gin.H{"error": "internal server error"})
+			return
+		}
+
+		// Access the expire_date value
+		if len(tokens) > 0 {
+			expireDate = tokens[0].ExpireDate.Format(time.RFC3339) // Convert time.Time to string
+			fmt.Println("Token expiration time:", expireDate)
+		}
+
+		c.Next()
 	}
 }
