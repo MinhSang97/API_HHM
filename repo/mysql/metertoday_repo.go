2f7404036e6c5eab6104fe522d37f25fd7eb0bc2
MinhSang97 <minhsangnguyen463@gmail.com>
2023-12-21 16:18:32 +0700

    API_HHM

--- repo/mysql/metertoday_repo.go
+++ repo/mysql/metertoday_repo.go
@@ -60,46 +60,78 @@ func (s meterTodayRepository) Search(ctx context.Context, meterAssetNo, Start_da
 		}
 	} else if meterModel == "HHM31/38" {
 		// Query for HHM31/38
-		query := `SELECT DISTINCT z.Ma_DIEMDO, z.tenkhachhang, z.nocongto, y.MR_TIME_FA thoigiandoc, z.dn_huucong_giao, z.dn_huucong_giao_bieu1, z.dn_huucong_giao_bieu2, z.dn_huucong_giao_bieu3,
-            z.dn_huucong_nhan, z.dn_huucong_nhan_bieu1, z.dn_huucong_nhan_bieu2, z.dn_huucong_nhan_bieu3, z.dn_vocong_giao, z.dn_vocong_giao_bieu1, z.dn_vocong_giao_bieu2, z.dn_vocong_giao_bieu3,
-            z.dn_vocong_nhan, z.dn_vocong_nhan_bieu1, z.dn_vocong_nhan_bieu2, z.dn_vocong_nhan_bieu3
-            FROM
-            (SElECT DISTINCT  e.Data_ID, e.Ma_DIEMDO, e.tenkhachhang, e.METER_ASSET_NO nocongto,e.TV thoigiandoc, e.dn_huucong_giao, e.dn_huucong_giao_bieu1, e.dn_huucong_giao_bieu2, e.dn_huucong_giao_bieu3,
-            e.dn_huucong_nhan, e.dn_huucong_nhan_bieu1, e.dn_huucong_nhan_bieu2, e.dn_huucong_nhan_bieu3, g.RA dn_vocong_giao, g.RA_T1 dn_vocong_giao_bieu1, g.RA_T2 dn_vocong_giao_bieu2, g.RA_T3 dn_vocong_giao_bieu3,
-            g.RR dn_vocong_nhan, g.RR_T1 dn_vocong_nhan_bieu1, g.RR_T2 dn_vocong_nhan_bieu2, g.RR_T3 dn_vocong_nhan_bieu3
-            FROM
-            (SELECT  DISTINCT d.Data_ID, CONS_NO Ma_DIEMDO, CONS_NAME tenkhachhang, METER_ASSET_NO, c.TV, FA dn_huucong_giao,FA_T1 dn_huucong_giao_bieu1,
-            FA_T2 dn_huucong_giao_bieu2, FA_T3 dn_huucong_giao_bieu3, FR dn_huucong_nhan,FR_T1 dn_huucong_nhan_bieu1, FR_T2 dn_huucong_nhan_bieu2, FR_T3 dn_huucong_nhan_bieu3
-            FROM A_Data_catalogue d
-            LEFT JOIN BIZ_PUB_DATA_F_ENERGY_D c 
-            ON d.Data_ID = c.Data_ID where d.METER_ASSET_NO = ? and c.RECEIVE_TIME LIKE ?)e
-            LEFT join BIZ_PUB_DATA_R_ENERGY_D g
-            ON e.Data_ID = g.Data_ID where e.METER_ASSET_NO = ? and g.tv LIKE ?)z
-            LEFT join BIZ_PUB_DATA_OTHER_D y
-            ON z.Data_ID = y.Data_ID and y.MR_TIME_FA LIKE ?` // Your query for HHM31/38 goes here
-		if err := s.db.Raw(query, meterAssetNo, End_date, meterAssetNo, End_date, End_date).
+		query := ` SELECT DISTINCT z.Ma_DIEMDO, z.tenkhachhang, z.nocongto, y.MR_TIME_FA thoigiandoc, 
+                            z.dn_huucong_giao, z.dn_huucong_giao_bieu1, z.dn_huucong_giao_bieu2, z.dn_huucong_giao_bieu3,
+                            z.dn_huucong_nhan, z.dn_huucong_nhan_bieu1, z.dn_huucong_nhan_bieu2, z.dn_huucong_nhan_bieu3, 
+                            z.dn_vocong_giao, z.dn_vocong_giao_bieu1, z.dn_vocong_giao_bieu2, z.dn_vocong_giao_bieu3,
+                            z.dn_vocong_nhan, z.dn_vocong_nhan_bieu1, z.dn_vocong_nhan_bieu2, z.dn_vocong_nhan_bieu3
+                FROM
+                    (SELECT DISTINCT e.Data_ID, e.Ma_DIEMDO, e.tenkhachhang, e.METER_ASSET_NO nocongto, e.tv, 
+                            e.dn_huucong_giao, e.dn_huucong_giao_bieu1, e.dn_huucong_giao_bieu2, e.dn_huucong_giao_bieu3,
+                            e.dn_huucong_nhan, e.dn_huucong_nhan_bieu1, e.dn_huucong_nhan_bieu2, e.dn_huucong_nhan_bieu3, 
+                            g.RA dn_vocong_giao, g.RA_T1 dn_vocong_giao_bieu1, g.RA_T2 dn_vocong_giao_bieu2, g.RA_T3 dn_vocong_giao_bieu3,
+                            g.RR dn_vocong_nhan, g.RR_T1 dn_vocong_nhan_bieu1, g.RR_T2 dn_vocong_nhan_bieu2, g.RR_T3 dn_vocong_nhan_bieu3
+                    FROM
+                        (SELECT DISTINCT d.Data_ID, CONS_NO Ma_DIEMDO, CONS_NAME tenkhachhang, METER_ASSET_NO, c.tv, 
+                                FA dn_huucong_giao, FA_T1 dn_huucong_giao_bieu1, FA_T2 dn_huucong_giao_bieu2, FA_T3 dn_huucong_giao_bieu3, 
+                                FR dn_huucong_nhan, FR_T1 dn_huucong_nhan_bieu1, FR_T2 dn_huucong_nhan_bieu2, FR_T3 dn_huucong_nhan_bieu3
+                        FROM A_Data_catalogue d
+                        LEFT JOIN BIZ_PUB_DATA_F_ENERGY_D c ON d.Data_ID = c.Data_ID 
+                        WHERE d.METER_ASSET_NO = ?
+                AND TRUNC(c.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                            AND TRUNC(c.tv) <= TO_DATE(?, 'yyyy-mm-dd')
+                        ) e
+                    LEFT JOIN BIZ_PUB_DATA_R_ENERGY_D g ON e.Data_ID = g.Data_ID 
+                    WHERE e.METER_ASSET_NO = ?
+                        AND g.TV > TO_DATE(?, 'yyyy-mm-dd') 
+                        AND g.TV < TO_DATE(?, 'yyyy-mm-dd')
+                    ) z
+                LEFT JOIN BIZ_PUB_DATA_OTHER_D y
+                    ON z.Data_ID = y.Data_ID and z.tv = y.tv
+                    WHERE z.nocongto = ?
+                    AND TRUNC(y.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                    AND TRUNC(y.tv) <= TO_DATE(?, 'yyyy-mm-dd')
+                        AND TRUNC(z.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                    AND TRUNC(z.tv) <= TO_DATE(?, 'yyyy-mm-dd')` // Your query for HHM31/38 goes here
+		if err := s.db.Raw(query, meterAssetNo, Start_date, End_date, meterAssetNo, Start_date, End_date, meterAssetNo, Start_date, End_date, Start_date, End_date).
 			Scan(&meters).Error; err != nil {
 
 			return nil, err
 		} else if meterModel == "HHM31" {
-			query := `SELECT DISTINCT z.Ma_DIEMDO, z.tenkhachhang, z.nocongto, y.MR_TIME_FA thoigiandoc, z.dn_huucong_giao, z.dn_huucong_giao_bieu1, z.dn_huucong_giao_bieu2, z.dn_huucong_giao_bieu3,
-            z.dn_huucong_nhan, z.dn_huucong_nhan_bieu1, z.dn_huucong_nhan_bieu2, z.dn_huucong_nhan_bieu3, z.dn_vocong_giao, z.dn_vocong_giao_bieu1, z.dn_vocong_giao_bieu2, z.dn_vocong_giao_bieu3,
-            z.dn_vocong_nhan, z.dn_vocong_nhan_bieu1, z.dn_vocong_nhan_bieu2, z.dn_vocong_nhan_bieu3
-            FROM
-            (SElECT DISTINCT  e.Data_ID, e.Ma_DIEMDO, e.tenkhachhang, e.METER_ASSET_NO nocongto,e.TV thoigiandoc, e.dn_huucong_giao, e.dn_huucong_giao_bieu1, e.dn_huucong_giao_bieu2, e.dn_huucong_giao_bieu3,
-            e.dn_huucong_nhan, e.dn_huucong_nhan_bieu1, e.dn_huucong_nhan_bieu2, e.dn_huucong_nhan_bieu3, g.RA dn_vocong_giao, g.RA_T1 dn_vocong_giao_bieu1, g.RA_T2 dn_vocong_giao_bieu2, g.RA_T3 dn_vocong_giao_bieu3,
-            g.RR dn_vocong_nhan, g.RR_T1 dn_vocong_nhan_bieu1, g.RR_T2 dn_vocong_nhan_bieu2, g.RR_T3 dn_vocong_nhan_bieu3
-            FROM
-            (SELECT  DISTINCT d.Data_ID, CONS_NO Ma_DIEMDO, CONS_NAME tenkhachhang, METER_ASSET_NO, c.TV, FA dn_huucong_giao,FA_T1 dn_huucong_giao_bieu1,
-            FA_T2 dn_huucong_giao_bieu2, FA_T3 dn_huucong_giao_bieu3, FR dn_huucong_nhan,FR_T1 dn_huucong_nhan_bieu1, FR_T2 dn_huucong_nhan_bieu2, FR_T3 dn_huucong_nhan_bieu3
-            FROM A_Data_catalogue d
-            LEFT JOIN BIZ_PUB_DATA_F_ENERGY_D c 
-            ON d.Data_ID = c.Data_ID where d.METER_ASSET_NO = ? and c.RECEIVE_TIME LIKE ?)e
-            LEFT join BIZ_PUB_DATA_R_ENERGY_D g
-            ON e.Data_ID = g.Data_ID where e.METER_ASSET_NO = ? and g.tv LIKE ?)z
-            LEFT join BIZ_PUB_DATA_OTHER_D y
-            ON z.Data_ID = y.Data_ID and y.MR_TIME_FA LIKE ?` // Your query for HHM31/38 goes here
-			if err := s.db.Raw(query, meterAssetNo, End_date, meterAssetNo, End_date, End_date).
+			query := ` SELECT DISTINCT z.Ma_DIEMDO, z.tenkhachhang, z.nocongto, y.MR_TIME_FA thoigiandoc, 
+                            z.dn_huucong_giao, z.dn_huucong_giao_bieu1, z.dn_huucong_giao_bieu2, z.dn_huucong_giao_bieu3,
+                            z.dn_huucong_nhan, z.dn_huucong_nhan_bieu1, z.dn_huucong_nhan_bieu2, z.dn_huucong_nhan_bieu3, 
+                            z.dn_vocong_giao, z.dn_vocong_giao_bieu1, z.dn_vocong_giao_bieu2, z.dn_vocong_giao_bieu3,
+                            z.dn_vocong_nhan, z.dn_vocong_nhan_bieu1, z.dn_vocong_nhan_bieu2, z.dn_vocong_nhan_bieu3
+                FROM
+                    (SELECT DISTINCT e.Data_ID, e.Ma_DIEMDO, e.tenkhachhang, e.METER_ASSET_NO nocongto, e.tv, 
+                            e.dn_huucong_giao, e.dn_huucong_giao_bieu1, e.dn_huucong_giao_bieu2, e.dn_huucong_giao_bieu3,
+                            e.dn_huucong_nhan, e.dn_huucong_nhan_bieu1, e.dn_huucong_nhan_bieu2, e.dn_huucong_nhan_bieu3, 
+                            g.RA dn_vocong_giao, g.RA_T1 dn_vocong_giao_bieu1, g.RA_T2 dn_vocong_giao_bieu2, g.RA_T3 dn_vocong_giao_bieu3,
+                            g.RR dn_vocong_nhan, g.RR_T1 dn_vocong_nhan_bieu1, g.RR_T2 dn_vocong_nhan_bieu2, g.RR_T3 dn_vocong_nhan_bieu3
+                    FROM
+                        (SELECT DISTINCT d.Data_ID, CONS_NO Ma_DIEMDO, CONS_NAME tenkhachhang, METER_ASSET_NO, c.tv, 
+                                FA dn_huucong_giao, FA_T1 dn_huucong_giao_bieu1, FA_T2 dn_huucong_giao_bieu2, FA_T3 dn_huucong_giao_bieu3, 
+                                FR dn_huucong_nhan, FR_T1 dn_huucong_nhan_bieu1, FR_T2 dn_huucong_nhan_bieu2, FR_T3 dn_huucong_nhan_bieu3
+                        FROM A_Data_catalogue d
+                        LEFT JOIN BIZ_PUB_DATA_F_ENERGY_D c ON d.Data_ID = c.Data_ID 
+                        WHERE d.METER_ASSET_NO = ?
+                AND TRUNC(c.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                            AND TRUNC(c.tv) <= TO_DATE(?, 'yyyy-mm-dd')
+                        ) e
+                    LEFT JOIN BIZ_PUB_DATA_R_ENERGY_D g ON e.Data_ID = g.Data_ID 
+                    WHERE e.METER_ASSET_NO = ?
+                        AND g.TV > TO_DATE(?, 'yyyy-mm-dd') 
+                        AND g.TV < TO_DATE(?, 'yyyy-mm-dd')
+                    ) z
+                LEFT JOIN BIZ_PUB_DATA_OTHER_D y
+                    ON z.Data_ID = y.Data_ID and z.tv = y.tv
+                    WHERE z.nocongto = ?
+                    AND TRUNC(y.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                    AND TRUNC(y.tv) <= TO_DATE(?, 'yyyy-mm-dd')
+                        AND TRUNC(z.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                    AND TRUNC(z.tv) <= TO_DATE(?, 'yyyy-mm-dd')` // Your query for HHM31/38 goes here
+			if err := s.db.Raw(query, meterAssetNo, Start_date, End_date, meterAssetNo, Start_date, End_date, meterAssetNo, Start_date, End_date, Start_date, End_date).
 				Scan(&meters).Error; err != nil {
 
 				return nil, err
@@ -107,32 +139,45 @@ func (s meterTodayRepository) Search(ctx context.Context, meterAssetNo, Start_da
 		}
 	} else if meterModel == "HHM38" {
 		// Default query or handle other cases
-		query := `SELECT DISTINCT z.Ma_DIEMDO, z.tenkhachhang, z.nocongto, y.MR_TIME_FA thoigiandoc, z.dn_huucong_giao, z.dn_huucong_giao_bieu1, z.dn_huucong_giao_bieu2, z.dn_huucong_giao_bieu3,
-		z.dn_huucong_nhan, z.dn_huucong_nhan_bieu1, z.dn_huucong_nhan_bieu2, z.dn_huucong_nhan_bieu3, z.dn_vocong_giao, z.dn_vocong_giao_bieu1, z.dn_vocong_giao_bieu2, z.dn_vocong_giao_bieu3,
-		z.dn_vocong_nhan, z.dn_vocong_nhan_bieu1, z.dn_vocong_nhan_bieu2, z.dn_vocong_nhan_bieu3
-	FROM
-	(SELECT DISTINCT  e.Data_ID, e.Ma_DIEMDO, e.tenkhachhang, e.METER_ASSET_NO nocongto, e.TV thoigiandoc,
-		e.dn_huucong_giao, e.dn_huucong_giao_bieu1, e.dn_huucong_giao_bieu2, e.dn_huucong_giao_bieu3,
-		e.dn_huucong_nhan, e.dn_huucong_nhan_bieu1, e.dn_huucong_nhan_bieu2, e.dn_huucong_nhan_bieu3,
-		g.RA dn_vocong_giao, g.RA_T1 dn_vocong_giao_bieu1, g.RA_T2 dn_vocong_giao_bieu2, g.RA_T3 dn_vocong_giao_bieu3,
-		g.RR dn_vocong_nhan, g.RR_T1 dn_vocong_nhan_bieu1, g.RR_T2 dn_vocong_nhan_bieu2, g.RR_T3 dn_vocong_nhan_bieu3
-	FROM
-	(SELECT DISTINCT d.Data_ID, CONS_NO Ma_DIEMDO, CONS_NAME tenkhachhang, METER_ASSET_NO, TV,
-		FA dn_huucong_giao, FA_T1 dn_huucong_giao_bieu1, FA_T2 dn_huucong_giao_bieu2, FA_T3 dn_huucong_giao_bieu3,
-		FR dn_huucong_nhan, FR_T1 dn_huucong_nhan_bieu1, FR_T2 dn_huucong_nhan_bieu2, FR_T3 dn_huucong_nhan_bieu3
-	FROM A_Data_catalogue d
-	LEFT JOIN BIZ_PUB_DATA_F_ENERGY_D c ON d.Data_ID = c.Data_ID WHERE d.METER_ASSET_NO = ? AND c.RECEIVE_TIME LIKE ?) e
-	LEFT JOIN BIZ_PUB_DATA_R_ENERGY_D g ON e.Data_ID = g.Data_ID) z
-	LEFT join BIZ_PUB_DATA_OTHER_D y
-	ON z.Data_ID = y.Data_ID and y.MR_TIME_FA LIKE ?` // Your default query goes here
-		if err := s.db.Raw(query, meterAssetNo, End_date, End_date).
+		query := ` SELECT DISTINCT z.Ma_DIEMDO, z.tenkhachhang, z.nocongto, y.MR_TIME_FA thoigiandoc, 
+                            z.dn_huucong_giao, z.dn_huucong_giao_bieu1, z.dn_huucong_giao_bieu2, z.dn_huucong_giao_bieu3,
+                            z.dn_huucong_nhan, z.dn_huucong_nhan_bieu1, z.dn_huucong_nhan_bieu2, z.dn_huucong_nhan_bieu3, 
+                            z.dn_vocong_giao, z.dn_vocong_giao_bieu1, z.dn_vocong_giao_bieu2, z.dn_vocong_giao_bieu3,
+                            z.dn_vocong_nhan, z.dn_vocong_nhan_bieu1, z.dn_vocong_nhan_bieu2, z.dn_vocong_nhan_bieu3
+                FROM
+                    (SELECT DISTINCT e.Data_ID, e.Ma_DIEMDO, e.tenkhachhang, e.METER_ASSET_NO nocongto, e.tv, 
+                            e.dn_huucong_giao, e.dn_huucong_giao_bieu1, e.dn_huucong_giao_bieu2, e.dn_huucong_giao_bieu3,
+                            e.dn_huucong_nhan, e.dn_huucong_nhan_bieu1, e.dn_huucong_nhan_bieu2, e.dn_huucong_nhan_bieu3, 
+                            g.RA dn_vocong_giao, g.RA_T1 dn_vocong_giao_bieu1, g.RA_T2 dn_vocong_giao_bieu2, g.RA_T3 dn_vocong_giao_bieu3,
+                            g.RR dn_vocong_nhan, g.RR_T1 dn_vocong_nhan_bieu1, g.RR_T2 dn_vocong_nhan_bieu2, g.RR_T3 dn_vocong_nhan_bieu3
+                    FROM
+                        (SELECT DISTINCT d.Data_ID, CONS_NO Ma_DIEMDO, CONS_NAME tenkhachhang, METER_ASSET_NO, c.tv, 
+                                FA dn_huucong_giao, FA_T1 dn_huucong_giao_bieu1, FA_T2 dn_huucong_giao_bieu2, FA_T3 dn_huucong_giao_bieu3, 
+                                FR dn_huucong_nhan, FR_T1 dn_huucong_nhan_bieu1, FR_T2 dn_huucong_nhan_bieu2, FR_T3 dn_huucong_nhan_bieu3
+                        FROM A_Data_catalogue d
+                        LEFT JOIN BIZ_PUB_DATA_F_ENERGY_D c ON d.Data_ID = c.Data_ID 
+                        WHERE d.METER_ASSET_NO = ?
+                AND TRUNC(c.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                            AND TRUNC(c.tv) <= TO_DATE(?, 'yyyy-mm-dd')
+                        ) e
+                    LEFT JOIN BIZ_PUB_DATA_R_ENERGY_D g ON e.Data_ID = g.Data_ID 
+                    WHERE e.METER_ASSET_NO = ?
+                        AND g.TV > TO_DATE(?, 'yyyy-mm-dd') 
+                        AND g.TV < TO_DATE(?, 'yyyy-mm-dd')
+                    ) z
+                LEFT JOIN BIZ_PUB_DATA_OTHER_D y
+                    ON z.Data_ID = y.Data_ID and z.tv = y.tv
+                    WHERE z.nocongto = ?
+                    AND TRUNC(y.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                    AND TRUNC(y.tv) <= TO_DATE(?, 'yyyy-mm-dd')
+                        AND TRUNC(z.tv) >= TO_DATE(?, 'yyyy-mm-dd')
+                    AND TRUNC(z.tv) <= TO_DATE(?, 'yyyy-mm-dd')` // Your query for HHM31/38 goes here
+		if err := s.db.Raw(query, meterAssetNo, Start_date, End_date, meterAssetNo, Start_date, End_date, meterAssetNo, Start_date, End_date, Start_date, End_date).
 			Scan(&meters).Error; err != nil {
 
 			return nil, err
 		}
 	}
-
-	// do something with result
 
 	return meters, nil
 }
